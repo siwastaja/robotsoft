bbf8eecba724759165791bbfc1072bc2d1e65be7
Antti Alhonen <antti.alhonen@iki.fi>
2019-07-19 12:28:18 +0300

    sensor calibration flashing script

---
+++ program_tofcals.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./spiprog tofcals/tofcal_00.bin tofcals/tofcal_01.bin tofcals/tofcal_02.bin tofcals/tofcal_03.bin tofcals/tofcal_04.bin tofcals/tofcal_05.bin tofcals/tofcal_06.bin tofcals/tofcal_07.bin tofcals/tofcal_08.bin tofcals/tofcal_09.bin
